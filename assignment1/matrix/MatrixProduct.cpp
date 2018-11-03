a4257fa7402d1a497c243d9956774d7bd8bb64ba
Rafael Ravedutti <rafael@laura-br.com>
2018-11-03 03:18:10 +0100

    Show error messages if there is any
    Signed-off-by: Rafael Ravedutti <rafael@laura-br.com>

--- assignment1/matrix/MatrixProduct.cpp
+++ assignment1/matrix/MatrixProduct.cpp
@@ -6,6 +6,11 @@ int main() {
 
   std::cin >> s1 >> s2 >> s3;
 
+  if(s1 * s2 * s3 == 0) {
+    std::cerr << "None of the input dimensions should be zero!" << std::endl;
+    return -1;
+  }
+
   Matrix m1(s1, s2, 0.0);
   Matrix m2(s2, s3, 0.0);
   Matrix m3(s1, s3, 0.0);
@@ -14,6 +19,11 @@ int main() {
   std::cin >> m2;
 
   m3 = m1 * m2;
+
+  if(m3.rows() * m3.cols() == 0) {
+    std::cerr << "An error occurred during the matrix product!" << std::endl;
+    return -1;
+  }
 
   std::cout << m3;
   return 0;
